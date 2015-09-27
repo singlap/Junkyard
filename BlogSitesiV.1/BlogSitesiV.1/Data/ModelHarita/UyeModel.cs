6e9bd5672e7fb86383f1de9bc1e2a9f7ed57f3b7
singlap <576576Sing!>
2015-09-27 14:03:39 +0300

    Uyemodel için Id ozelligini autoincrement yaptım

--- BlogSitesiV.1/BlogSitesiV.1/Data/ModelHarita/UyeModel.cs
+++ BlogSitesiV.1/BlogSitesiV.1/Data/ModelHarita/UyeModel.cs
@@ -1,6 +1,7 @@
 ﻿using BlogSitesiV._1.Data.Model;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Web;
@@ -15,6 +16,7 @@ namespace BlogSitesiV._1.Data.ModelHarita
             HasKey(t => t.uyeID);
 
             //Fields
+            Property(t => t.uyeID).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
             Property(t => t.uyeAd);
             Property(t => t.uyeSoyad);
             Property(t => t.uyeEmail);
