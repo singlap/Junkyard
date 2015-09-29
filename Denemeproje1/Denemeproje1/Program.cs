624a2897234680fc18b8526cf0684e2e55aead9f
singlap <576576Sing!>
2015-09-29 14:04:05 +0300

    access modifiers giriş ve kalıtım alma ufak denemeler

--- Denemeproje1/Denemeproje1/Program.cs
+++ Denemeproje1/Denemeproje1/Program.cs
@@ -6,8 +6,11 @@ using System.Threading.Tasks;
 
 namespace Denemeproje1
 {
-    class Program
+    
+    class Program : privateSinifiIcinDeneme1
     {
+        
+       
         static void Main(string[] args)
         {
             Console.WriteLine("merhaba dunya");
@@ -27,6 +30,31 @@ namespace Denemeproje1
             //- Inheritance (Miras, Kalıtım)
             //- Interface (Arayüz)
             //- Polymorphizm (Çok Biçimlilik)
+
+            /*Classes (Sınıflar) ve Objects (Nesneler)*/
+           // Access Modifier i public olan sınıf klasından  nesne adında bir nesne oluşturulup  propertilerine değer atandı.
+            sınıf nesne = new sınıf();
+
+            nesne.aDegeri = 1;
+            nesne.bDegeri = "asd";
+            // kalıtım alınarak nesne oluşturulabildiği gibi  aynı namespace içerisinde tanımlanan bir metotdan kalıtım alınmadan da nesne oluşturulabiliyormuş.
+            sınıf2 nesneOrnek = new sınıf2();
+            nesneOrnek.digerBiryontem = 1;
+            //sınıf2 metotdunun içerisinde tanımlanan digerBirYontem2 alanının access modifieri  private olduğu için o alana değer atayamıyoruz.
+            //nesneOrnek.digerBiryontem2 = "2";
+
+            // Access Modifier i private olan sınıf klasından  nesne2 adında bir nesne oluşturulup  propertilerine değer atandı.
+            //Fakat diğer class içinde private olarak tanımlandığı için  program classı içinde erişim sağlanamayacağından hata verecektir.
+            // Bu sebeple simdilik build edilebilmesi açısından yorum satırı haline getirildi.
+            //privateSınıf nesne2 = new privateSınıf();
+            //nesne2.aDegeri = 1;
+
+            /*Erişim belirliyiciler (Access Modifiers)*/
+
+            //-  Public : Bu erişim seviyesi belirtilmiş olan sınıflar, metotlar, alanlar sınırsız erişime sahiptir. Ve her yerden erişebiliriz.
+            //- Private : Bu erişim seviyesinde sadece tanımlanan yerden erişebilir. Yani sınıf içinde tanımlandıysa sadece o sınıfta erişim sağlanır.
+            //  Başka bir sınıfta kullanılamaz.
         }
+       
     }
 }
---
+++ Denemeproje1/Denemeproje1/privateSinifiIcinDeneme1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Denemeproje1 
+{
+    class privateSinifiIcinDeneme1 
+    {
+        // sınıf yaratıldı
+        public class sınıf
+        {
+            //Fields(Alanlar) ve (Properties)Özellikleri belirtildi
+            public int aDegeri { get; set; }
+            public string bDegeri { get; set; }
+
+        }
+        // sınıf yaratıldı
+        private class privateSınıf
+        {
+            //Fields(Alanlar) ve (Properties)Özellikleri belirtildi
+            public int aDegeri { get; set; }
+            public string bDegeri { get; set; }
+
+        }
+
+    }
+    // "aynı namespace içerisinde tanımlanan sınıflardan kalıtım almadan  nesne oluşturulabiliyormu? " örneği
+     class sınıf2
+    {
+        public int digerBiryontem { get; set; }
+        private string digerBirYontem2 { get; set; }
+    }
+}
